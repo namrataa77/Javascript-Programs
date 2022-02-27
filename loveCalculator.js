777156722689c77a52090546bcd007828af9890d
namrata77 <namratathakur077@gmail.com>
2022-02-27 10:59:57 +0530

    calculate love percentage

---
+++ loveCalculator.js
@@ -0,0 +1,18 @@
+function love(name1,name2)
+{
+
+
+
+var name1 = prompt("enter your name : ");
+var name2 = prompt("enter your crush's name : ");
+
+var n = Math.random();
+
+n = n*100;
+
+alert("Love percentage of " + name1 + " and " + name2 + " is : " + n);
+
+}
+
+
+love();
