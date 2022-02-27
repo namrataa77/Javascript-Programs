0dc5a06f98e05f5eec25d1f52bdffa14d428b37c
namrata77 <namratathakur077@gmail.com>
2022-02-27 09:42:16 +0530

    calculating life in days, weeks and years

---
+++ lifeInWeeks.js
@@ -0,0 +1,21 @@
+
+    
+var age = prompt("What is your age? ");
+
+var years = 90 - age;
+document.write("<br>");
+document.write(years);
+
+var days = years * 365;
+document.write("<br>");
+document.write(days);
+
+const weeks = days / 7;
+document.write("<br>");
+document.write(weeks);
+
+
+
+
+    
+    
---
+++ randomtext.js
@@ -0,0 +1,27 @@
+alert("hi there!");
+document.write(typeof("namrata"));
+
+var myname = "Namrata";
+var username = prompt("What is your name?");
+
+alert("my name is " + myname + ". Your name is  " + username);
+
+//length of a string
+document.write("<br>");
+document.write(myname.length);
+
+//slicing parts of a string
+myname.slice(0,4);
+document.write("<br>");
+document.write(myname);
+
+//changing case of text
+myname = "namrata";
+document.write("<br>");
+myname = myname.toUpperCase(); 
+document.write(myname);
+//myname = myname.toLowerCase();
+
+
+
+
